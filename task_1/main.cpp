6a72f73cfe7fbe88bee6684b40d565bb1b9aa8b5
AKorezin <source-email@yandex.ru>
2014-03-01 19:27:14 +0400

    task_1 completed

---
+++ task_1/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+
+
+int main(int argc,char **argv)
+{
+	std::ifstream ifs(argv[1], std::ifstream::in);
+	std::string sbuf;//,pattern="\\d*";
+	//std::regex rx ("\\d+", std::regex::ECMAScript|std::regex::icase);
+	//std::smatch m;
+	int n;
+	ifs>>n;
+	std::list<double> x,y;
+	std::list<double>::iterator itx,ity;
+	double temp;
+	for(int i=0;i<n;i++)
+	{
+		ifs>>temp;
+		x.push_back(temp);
+	}
+	for(int i=0;i<n;i++)
+	{
+    	ifs>>temp;
+		y.push_back(temp);
+	}
+	ifs>>temp;
+#if (DEBUG)
+	for	(itx=x.begin(),ity=y.begin(); itx!=x.end() && ity!=y.end(); itx++, ity++)
+	{
+		std::cout<<*itx<<" "<<*ity<<" "<<std::endl;
+	}
+	std::cout<<temp<<std::endl;
+#endif
+	
+	std::list<double>::iterator itx1;
+	double l=1,res=0;
+	for(itx=x.begin(),ity=y.begin();itx!=x.end();itx++,ity++)
+	{
+		for(itx1=x.begin();itx1!=x.end();itx1++)
+			if(itx1!=itx)
+			{
+				l*=(temp-*itx1)/(*itx-*itx1);
+			}
+		res+=l*(*ity);
+		l=1;
+	}
+	std::cout<<res<<std::endl;
+	/*for(i=1;!ifs.eof();i++)
+	{
+		std::getline(ifs,sbuf,'\n');
+		if(!ifs.eof())
+		{
+			while(std::regex_search (sbuf,m,rx))
+			{
+				for(auto x:m)
+					std::cout << x << " ";
+				std::cout<<std::endl;
+				sbuf=m.suffix().str();
+			}
+			
+
+		}
+	}*/
+
+	return 0;
+
+}
